5c159cc8ab03271ac76a1bcb26879208d77e002f
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-20 02:29:03 +0500

    Auto update

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,18 +1,15 @@
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onINP(onPerfEntry); // FID → INP (updated)
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
 
 export default reportWebVitals;
-
-// 👇 yeh add karo
-export {};
