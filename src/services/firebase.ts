9341b26063ed03b7b6789a12b389a36bd0bbee64
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-20 03:11:58 +0500

    Auto update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCqlkbr-PeJIMv5SgvadI5eazN1ztRS6ac",
+  authDomain: "glazeme-46406.firebaseapp.com",
+  projectId: "glazeme-46406",
+  storageBucket: "glazeme-46406.firebasestorage.app",
+  messagingSenderId: "885518291591",
+  appId: "1:885518291591:web:69097ce591d5b81d939e22",
+  measurementId: "G-325LJXD1YK"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
