708487b51549849f1cdce8cd03da62cbc8b6ea1c
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-20 18:37:02 +0500

    Auto update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // src/App.tsx
 import React from 'react';
 import Dashboard from './components/Dashboard';
-
+import BuildUpdatesContainer from './components/BuildUpdatesContainer';
 function App() {
   return (
     <div className="App">
+            <BuildUpdatesContainer />
+
       <Dashboard />
     </div>
   );
