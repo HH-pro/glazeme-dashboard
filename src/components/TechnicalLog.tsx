e866e7420b152752cdac49503c83053b2cf5a947
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-20 15:47:20 +0500

    Auto update

--- src/components/TechnicalLog.tsx
+++ src/components/TechnicalLog.tsx
@@ -223,17 +223,7 @@ const TechnicalLog: React.FC<Props> = ({ isEditMode = false, onEditAction }) => 
       {/* Technical Milestones Tracker */}
       <div style={styles.milestonesSection}>
         <div style={styles.milestonesHeader}>
-          <h3 style={styles.sectionSubtitle}>📋 Technical Milestones</h3>
-          <button 
-            onClick={handleAddClick}
-            style={{
-              ...styles.addButton,
-              backgroundColor: isEditMode ? '#FF8C42' : '#6c757d',
-              cursor: isEditMode ? 'pointer' : 'not-allowed'
-            }}
-          >
-            {isEditMode ? '+ Add Milestone' : '🔒 Edit Mode Required'}
-          </button>
+         
         </div>
 
         {isEditMode && showAddForm && (
